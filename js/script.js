9c16e388d31039101917f3d7ea7c2101e9427b4c
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-03-09 18:34:30 +0100

    added file css and elements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const app = document.getElementById('app');
+
 const ageBox = app.querySelector('input[name="age"]');
 console.dir(ageBox);
 const kmBox = app.querySelector('input[name="km"]');
@@ -8,7 +10,7 @@ console.log(buttonCalc)
 const buttonCanc = document.getElementById('btnCanc');
 console.log(buttonCanc);
 
-const ticket = document.querySelector('ticket');
+const ticket = document.querySelector('.ticket');
 console.log(ticket);
 
 
@@ -16,16 +18,36 @@ buttonCalc.addEventListener('click', function() {
     let age = ageBox.value;
     let km = kmBox.value;
     let pricexKm = 0.21;
-    let price = km * pricexKm;
-    ticket.innerHTML += `<h3>${price}€</h3>`;
+    let ticketPrice = km * pricexKm;
+    const discountMinors = ticketPrice * 0.20;
+    const discountOver = ticketPrice* 0.40;
+    if (age < 18) {
+        totalPrice = ticketPrice - discountMinors;
+        
+    } else if (age > 64) {
+        totalPrice = ticketPrice - discountOver;
+        
+    }
+    else {
+        totalPrice = ticketPrice;
+        
+    };
+    ticket.innerHTML += `<h6>${totalPrice.toFixed()} €</h6>`;
     ticket.classList.remove('d-none');
 
 });
+console.log(buttonCalc);
 
-buttonCalc.addEventListener('click', function() {
+
+buttonCanc.addEventListener('click', function() {
     ageBox.value = '';
     kmBox.value = '';
     ticket.innerHTML = '';
     ticket.classList.add('d-none');
 
 });
+console.log(buttonCalc);
+
+
+
+
